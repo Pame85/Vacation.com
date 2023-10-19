ae8709dd7379f6cb300ec3e73fb6e6bec59c1a10
Pame85 <pamellachua85@gmail.com>
2023-10-19 15:52:30 +0800

    booking changes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,6 +10,7 @@ class Booking < ApplicationRecord
  attribute :check_in, :date
 
   private
+  
 
   def check_in_cannot_be_in_the_past
     errors.add(:check_in, "can't be in the past") if check_in.present? && check_in < Date.today
@@ -20,9 +21,10 @@ class Booking < ApplicationRecord
   end
 
   def check_booking_overlap
-    overlapping_booking = listing.booking.where('(check_in <= ? AND check_out >= ?) OR (check_in <= ? AND check_out >= ?)',
+    overlapping_bookings = listing.bookings.where('(check_in <= ? AND check_out >= ?) OR (check_in <= ? AND check_out >= ?)',
                                                    check_in, check_in,
                                                    check_out, check_out)
     errors.add(:base, "Booking dates overlap with existing bookings") if overlapping_bookings.exists?
   end
+  
 end
