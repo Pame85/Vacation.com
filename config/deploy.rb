cc4b25649386e9b2aef52681274b7ee60e3ab625
Pame85 <pamellachua85@gmail.com>
2023-10-19 13:17:13 +0800

    git repo url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '~> 3.18.0'
 
 # Change these
-set :repo_url,        'git@github.com:Pame85/Vacation.com.git'
+set :repo_url,        'https://github.com/Pame85/Vacation.com.git'
 set :user,            'deploy'
 
 # Don't change these unless you know what you're doing
@@ -16,8 +16,8 @@ set :log_level,     :debug
 set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb', 'config/master.key', 'sockets/puma.sock')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'sockets')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb', 'config/master.key')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Skip Sprockets build in favour of Webpacker.
 Rake::Task['deploy:assets:backup_manifest'].clear_actions
